ea4876d1b5e10ce527594edb2d748e5d73df9114
PERSIA-MISI <124380548+PERSIA-MISI@users.noreply.github.com>
2024-12-14 01:12:04 +0330

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -4,9 +4,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '052f44f9-eadd-46d1-8b64-e9b9b15ddea4';
 
-let proxyIP = '';
+let proxyIP = '45.81.225.142';
 
 
 if (!isValidUUID(userID)) {
